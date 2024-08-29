dc63b935f8e4b7b1d9eb0c721e94539f8ac27d17
Lucy0112358 <lusinehsahakian@gmail.com>
2024-08-29 23:02:59 +0400

    Added exception handling mechanism

---
+++ MqttService/Enums/MqttErrorCodeEnum.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace MqttService.Enums
+{
+    public enum MqttErrorCodeEnum : long
+    {
+        [Description("An error occured, please try again later")]
+        GenericErrorRetry = 1,
+
+        [Description("Account has been deleted and cannot be used anymore")]
+        UserAccountIsDeleted = 2,
+
+        [Description("The password is invalid. Please make sure you request the right locker or contact support.")]
+        InvalidPassword = 11,
+
+        [Description("The card is invalid. Please make sure you request the right locker or contact support.")]
+        InvalidCard = 12,
+
+        [Description("Success.")]
+        Success = 100,
+
+    }
+}
