cf0485d637115985a3ae625595b6b2d8f8f295fc
Lucy0112358 <lusinehsahakian@gmail.com>
2024-09-08 18:51:26 +0400

    added methods to get brainmodules and branches

---
+++ Domain/Entities/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Branch
+    {
+        [Column]
+        public int Id { get; set; }
+    }
+}
