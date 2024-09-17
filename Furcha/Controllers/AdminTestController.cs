e91c7f4aed297ba863218549a4cb6ca52329a5dc
Lucy0112358 <lusinehsahakian@gmail.com>
2024-09-17 22:51:02 +0400

    test

--- Furcha/Controllers/AdminTestController.cs
+++ Furcha/Controllers/AdminTestController.cs
@@ -4,16 +4,16 @@ namespace Furcha.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class AdminTestController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<AdminTestController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public AdminTestController(ILogger<AdminTestController> logger)
         {
             _logger = logger;
         }
