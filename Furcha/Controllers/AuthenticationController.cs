c995e790a4f6f56b58a11309ba54d627c53b98c1
Lucy0112358 <lusinehsahakian@gmail.com>
2024-09-20 15:54:13 +0400

    lb - add: addeed the endpoint and model for login functionality

---
+++ Furcha/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using FurchaAdminApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurchaAdminApi.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        [HttpPost]
+        [AllowAnonymous]
+        public string Login([FromBody] AuthenticateRequest authenticateRequest)
+        {
+            var token = authenticationService.LoginToGetJwtToken(authenticateRequest);
+
+            return token;
+        }
+    }
+}
