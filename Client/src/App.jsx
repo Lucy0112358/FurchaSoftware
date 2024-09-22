cec6c1523f41d97080adf0472b53256dd4c2efc4
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-09-22 17:05:38 +0400

    add auth endpoint

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,18 +3,27 @@ import './App.css';
 import Modules from './pages/modules/Modules';
 import Layout from './components/layout/Layout';
 import Signin from './pages/auth/Signin';
+import PrivateRoute from './middleware/PrivateRoute';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+
 
 function App() {
 
   return (
+    <>
+    <ToastContainer />
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Modules />} />
+        <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
+          <Route index element={<PrivateRoute><Modules /></PrivateRoute>} />
         </Route>
        <Route path="login" element={<Signin />}></Route>
       </Routes>
     </Router>
+    </>
+    
   )
 }
 
---
+++ Client/src/redux/api/authApi.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import instance from "../../config/axios/axiosConfig";
+
+export const signin = createAsyncThunk(
+  "auth/signin",
+  async (data, thunkAPI) => {
+    try {
+        console.log(data, "Dataaaaaaaaaaaaaaaa");
+      const signInData = {
+        email: data.email,
+        password: data.password,
+      };
+
+      const config = {
+        method: "post",
+        url: "auth/login",
+        data: signInData,
+      };
+
+      const response = await instance(config);
+      console.log(response, "responsewwwwwwwwwwwwwwwwwww")
+      // localStorage.setItem("token", response.data.access_token);
+    //   window.location.href = `/users`;
+      return true;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.error.both);
+    }
+  }
+);
+
+// export const getCurrentUser = createAsyncThunk(
+//   'auth/getCurrentUser',
+//   async (_, thunkAPI) => {
+//       try {
+//         const config = {
+//           method: "get",
+//           url: 'auth/me',
+//         };
+        
+//         const response = await instance(config);
+//         return response?.data;
+//       } catch (error) {
+//         return thunkAPI.rejectWithValue(error.response.data.error.both);
+//       }
+//     }
+// )
